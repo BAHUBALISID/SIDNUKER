693ff5a44f9ab332979473d421e3a9d185104379
ShashwatSen <114056461+ShashwatSen@users.noreply.github.com>
2024-07-14 21:04:54 +0530

    V1.0.0

---
+++ config.py
@@ -0,0 +1,42 @@
+SERVER_CONFIG = {
+    "new_name": "nuked by sid",  # New Server Name here      
+    "new_icon": "sid here babe nextra here mfs you get fucked",   # New Server Icon url here → https://yourimage.jpg    
+    "new_description": "nuked by kol sid cry hard",  # New Server Description here    
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "YOUR SID",  # Webhook Name here     
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 50,  # Number of channels/ better max 50    
+    'channel_type': 'text',  # text/voice     
+    'channel_name': 'NUKED BY SID',  # Channel name      
+    'num_messages': 30,  # Number of message to spam/better max 30     
+    'message_content': '@everyone JOIN  https://discord.gg/u872zDpf6V'# Spam Message     
+}
+
+NO_BAN_KICK_ID = {
+    1027444990139453484,       # Put Whitelist ID
+    1027444990139453484,       # No Ban & No Kick
+    1027444990139453484,
+}
+
+BOT_PRESENCE = {
+    "type": "streaming",  # "playing", "listening", or "watching"
+    "text": "sid_xD"  # you status like playing sid_xD
+}
+
+EMBED_CONFIG = {
+    "title": "SidOwned",    # Maine Embed Title here you can change it :3          
+    "description": "SidOwned",   # Main Embed Description here/ description of the discord server           
+    "color": 0xFF5733,   # Change embed color if you want (red) is 0xFF5733        
+    "fields": [
+        {"name": "SidOwned", "value": "SidOwned", "inline": False},     
+        {"name": "KolSidOwned", "value": "SidOwned", "inline": False},
+        {"name": "SidOwned", "value": "SidOwned", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False      
+    ],
+    "image": "   sid owned you motherfuckers",   # Embed Icon url here → https://yourimage.com     
+    "footer": "SidOwned",  # Embed Footer here/ just a text     
+}
